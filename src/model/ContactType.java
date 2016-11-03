3896d1501537e7d38160379b68645e4b7e5f7131
tosha220 <tosha-220@yandex.ru>
2016-11-03 18:33:50 +0300

    ContactType edited

--- src/model/ContactType.java
+++ src/model/ContactType.java
@@ -1,10 +1,19 @@
 package model;
 
 public enum ContactType {
-    PHONE,
-    MOBILE,
-    HOME_PAGE,
-    SKYPE,
-    MAIL,
-    ICQ
+    PHONE("Тел"),
+    MOBILE("Мобильный тел"),
+    HOME_PAGE("Домашняя страница"),
+    SKYPE("Skype"),
+    MAIL("Mail"),
+    ICQ("ICQ");
+    private String title;
+
+    ContactType(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
